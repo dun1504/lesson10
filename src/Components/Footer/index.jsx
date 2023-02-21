92f021951866faeea5850185aaacb4f0d91c0948
Teo <teo@gmail.com>
2023-02-21 21:21:03 +0700

    fc

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css'
+const Footer = () => {
+  return (
+    <div> <p>&copy; Dune 2023</p></div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Shoppe</h1>
+      <ul>
+        <li>Home</li>
+        <li>Page</li>
+        <li>About</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
